b353155cb7b42613cf94610f116791e55b08b226
Timur Shatanov <58140315+shatanov@users.noreply.github.com>
2020-10-10 10:06:15 +0300

    Update app.js
    first chart

--- js/app.js
+++ js/app.js
@@ -1,19 +1,35 @@
-var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
+const ctx = document.getElementById('ChartWeek').getContext('2d');
+
+let dataElem = [10, 10, 20, 10];
+var mixedChart = new Chart(ctx, {
     type: 'bar',
-
-    // The data for our dataset
     data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
         datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
-        }]
+            label: 'Фактическая',
+            data: dataElem,
+             backgroundColor: [
+                '#F4B802',
+                '#F4B802',
+                '#F4B802',
+                '#F4B802',
+                '#F4B802',
+            ],
+            order: 1
+            }, {
+            label: 'План',
+            data: [45,45,45],
+            type: 'line',
+            strokeColor: 'black',
+            pointColor: 'black',
+            backgroundColor: [
+                'rgba(0,0,0,0)'
+            ],
+            borderColor: [
+                '#4279BC'
+            ],
+            order: 2
+        }],
+        labels: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Cб', 'Вс']
     },
-
-    // Configuration options go here
     options: {}
 });
